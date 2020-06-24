08f5869f14764486d50dd1be0a311ed1781b4922
seturi <48719589+seturi@users.noreply.github.com>
2020-06-24 12:42:02 +0900

    16th Commit

---
+++ CS DataStructure/07 Tree/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07_Tree
+{
+    // 이진 트리 노드 클래스
+    public class BinaryTreeNode<T>
+    {
+        public T Data { get; set; }
+        public BinaryTreeNode<T> Left { get; set; }
+        public BinaryTreeNode<T> Right { get; set; }
+
+        public BinaryTreeNode(T data)
+        {
+            this.Data = data;
+        }
+    }
+
+    // 이진 트리 클래스
+    public class BinaryTree<T>
+    {
+        public BinaryTreeNode<T> Root { get; set; }
+
+        // 트리 데이터 출력 예
+        public void PreOrderTraversal(BinaryTreeNode<T> node)
+        {
+            if (node == null) return;
+
+            Console.WriteLine(node.Data);
+            PreOrderTraversal(node.Left);
+            PreOrderTraversal(node.Right);
+        }
+    }
+
+    // 테스트 예제
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BinaryTree<int> btree = new BinaryTree<int>();
+            btree.Root = new BinaryTreeNode<int>(1);
+            btree.Root.Left = new BinaryTreeNode<int>(2);
+            btree.Root.Right = new BinaryTreeNode<int>(3);
+            btree.Root.Left.Left = new BinaryTreeNode<int>(4);
+
+            btree.PreOrderTraversal(btree.Root);
+        }
+    }
+}
---
+++ CS DataStructure/08 BinarySearchTree/Program.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08_BinarySearchTree
+{
+    // 이진 트리 노드 클래스
+    public class BinaryTreeNode<T>
+    {
+        public T Data { get; set; }
+        public BinaryTreeNode<T> Left { get; set; }
+        public BinaryTreeNode<T> Right { get; set; }
+
+        public BinaryTreeNode(T data)
+        {
+            this.Data = data;
+        }
+    }
+
+    // 이진 트리 클래스
+    public class BinaryTree<T>
+    {
+        public BinaryTreeNode<T> Root { get; set; }
+
+        // 트리 데이터 출력 예
+        public void PreOrderTraversal(BinaryTreeNode<T> node)
+        {
+            if (node == null) return;
+
+            Console.WriteLine(node.Data);
+            PreOrderTraversal(node.Left);
+            PreOrderTraversal(node.Right);
+        }
+    }
+
+    // 이진검색트리 클래스
+    public class BST<T>
+    {
+        private BinaryTreeNode<T> root = null;
+        private Comparer<T> comparer = Comparer<T>.Default;
+
+        public void Insert(T val)
+        {
+            BinaryTreeNode<T> node = root;
+            if (node == null)
+            {
+                root = new BinaryTreeNode<T>(val);
+                return;
+            }
+
+            while (node != null)
+            {
+                int result = comparer.Compare(node.Data, val);
+                if (result == 0)
+                {
+                    //throw new InvalidDataException("Duplicate value");
+                    return;
+                }
+                else if (result > 0)
+                {
+                    if (node.Left == null)
+                    {
+                        node.Left = new BinaryTreeNode<T>(val);
+                        return;
+                    }
+                    node = node.Left;
+                }
+                else
+                {
+                    if (node.Right == null)
+                    {
+                        node.Right = new BinaryTreeNode<T>(val);
+                        return;
+                    }
+                    node = node.Right;
+                }
+            }
+        }
+
+        public void PreOrderTraversal()
+        {
+            PreOrderRecursive(root);
+        }
+
+        private void PreOrderRecursive(BinaryTreeNode<T> node)
+        {
+            if (node == null) return;
+            Console.WriteLine(node.Data);
+            PreOrderRecursive(node.Left);
+            PreOrderRecursive(node.Right);
+        }
+    }
+
+    // 이진검색트리 테스트
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            // 1. BST 구현
+            BST<int> bst = new BST<int>();
+            bst.Insert(4);
+            bst.Insert(2);
+            bst.Insert(6);
+            bst.Insert(1);
+            bst.Insert(7);
+
+            bst.PreOrderTraversal();
+
+
+            // 2. SortedDictionary<Tkey,TValue> 클래스
+            SortedDictionary<int, string> tmap = new SortedDictionary<int, string>();
+            tmap.Add(1001, "Tom");
+            tmap.Add(1003, "John");
+            tmap.Add(1010, "Irina");
+
+            // Key 1010의 데이타 읽기
+            string name1010 = tmap[1010];
+
+            // Iterator 사용
+            foreach (KeyValuePair<int, string> kv in tmap)
+            {
+                Console.WriteLine("{0}:{1}", kv.Key, kv.Value);
+            }
+        }
+    }
+}
